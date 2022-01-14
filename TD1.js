34deae0dd8441f72a9b743c1c9c141a7f232d04c
Elyes <elyess.laiche@gmail.com>
2022-01-14 10:32:19 +0100

    Quest 5 & 6

--- TD1.js
+++ TD1.js
@@ -27,9 +27,32 @@
 function PrintMultip1To10() { //Quest4
     for (let i = 0; i <= 10; i++) {
         for (let j = 0; j <= 10; j++) {
-            console.log(j* i)
+            console.log(j * i)
         }
     }
 };
 console.log("--------------------------------------------")
 PrintMultip1To10()
+
+function PrintSum(nbr) { //Quest5
+    if (nbr >= 1){
+        let result = nbr + PrintSum(nbr - 1);
+        return result
+    }
+    else
+        return 0;
+};
+console.log("--------------------------------------------")
+console.log(PrintSum(10))
+
+function PrintFact(nbr) { //Quest6
+    if (nbr >= 1){
+        let result = nbr * PrintFact(nbr - 1);
+        return result
+    }
+    else
+        return 1;
+};
+console.log("--------------------------------------------")
+console.log(PrintFact(10))
+
