1ca65b9710d442ac050a026f064d13b4208e3ee3
Elyes <elyess.laiche@gmail.com>
2022-01-14 10:19:31 +0100

    Quest 3&4

--- TD1.js
+++ TD1.js
@@ -1,16 +1,35 @@
 "use strict"
 // function Print1To10() {   // Quest1
-//     for (let i = 1 ; i <= 10; i++) {
+//     for (let i = 1; i <= 10; i++) {
 //         console.log(i)
 //     }
 // };
 
 // Print1To10()
 
-function PrintOddTo100() { // Quest2
-    for (let i = 1 ; i <= 50; i++) {
-        console.log(2*i)
+// function PrintOddTo100() { // Quest2
+//     for (let i = 1; i <= 50; i++) {
+//         console.log(2 * i)
+//     }
+// };
+// console.log("--------------------------------------------")
+// PrintOddTo100()
+
+
+// function PrintMultip7() { //Quest3
+//     for (let i = 0; i <= 9; i++) {
+//         console.log(7 * i)
+//     }
+// };
+// console.log("--------------------------------------------")
+// PrintMultip7()
+
+function PrintMultip1To10() { //Quest4
+    for (let i = 0; i <= 10; i++) {
+        for (let j = 0; j <= 10; j++) {
+            console.log(j* i)
+        }
     }
 };
-
-PrintOddTo100()
+console.log("--------------------------------------------")
+PrintMultip1To10()
