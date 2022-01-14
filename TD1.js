c9521a3b7497e73525ea50f16acaabb7241d1f40
Elyes <elyess.laiche@gmail.com>
2022-01-14 10:53:39 +0100

    Quest 8 & 9

--- TD1.js
+++ TD1.js
@@ -9,7 +9,7 @@
 
 // function PrintOddTo100() { // Quest2
 //     for (let i = 1; i <= 50; i++) {
-//         console.log(2 * i)
+//         console.log(2 * i + 1)
 //     }
 // };
 // console.log("--------------------------------------------")
@@ -24,35 +24,56 @@
 // console.log("--------------------------------------------")
 // PrintMultip7()
 
-function PrintMultip1To10() { //Quest4
-    for (let i = 0; i <= 10; i++) {
-        for (let j = 0; j <= 10; j++) {
-            console.log(j * i)
-        }
-    }
-};
-console.log("--------------------------------------------")
-PrintMultip1To10()
+// function PrintMultip1To10() { //Quest4
+//     for (let i = 0; i <= 10; i++) {
+//         for (let j = 0; j <= 10; j++) {
+//             console.log(j * i)
+//         }
+//     }
+// };
+// console.log("--------------------------------------------")
+// PrintMultip1To10()
 
-function PrintSum(nbr) { //Quest5
-    if (nbr >= 1){
-        let result = nbr + PrintSum(nbr - 1);
-        return result
-    }
-    else
-        return 0;
-};
-console.log("--------------------------------------------")
-console.log(PrintSum(10))
+// function PrintSum(nbr) { //Quest5
+//     if (nbr >= 1){
+//         let result = nbr + PrintSum(nbr - 1);
+//         return result
+//     }
+//     else
+//         return 0;
+// };
+// console.log("--------------------------------------------")
+// console.log(PrintSum(10))
 
-function PrintFact(nbr) { //Quest6
-    if (nbr >= 1){
-        let result = nbr * PrintFact(nbr - 1);
-        return result
-    }
-    else
-        return 1;
-};
-console.log("--------------------------------------------")
-console.log(PrintFact(10))
+// function PrintFact(nbr) { //Quest6
+//     if (nbr >= 1){
+//         let result = nbr * PrintFact(nbr - 1);
+//         return result
+//     }
+//     else
+//         return 1;
+// };
+// console.log("--------------------------------------------")
+// console.log(PrintFact(10))
 
+// function PrintSum10To30() { //Quest7
+//     let result = 0
+//     for(var i = 10; i <= 30; i+=2){        
+//         result = result + i;
+//     }
+//     console.log(result)
+// };
+// console.log("--------------------------------------------")
+// PrintSum10To30()  
+
+// function PrintDegToFar(nbr) { //Quest8
+//    console.log(nbr* 9/5 + 32)
+// };
+// console.log("--------------------------------------------")
+// PrintDegToFar(20) 
+
+// function PrintFarToDeg(nbr) { //Quest9
+//     console.log((nbr- 32) * 5/9)
+//  };
+//  console.log("--------------------------------------------")
+// PrintFarToDeg(20) 
