ecce331ecc38c01e3df6ac3dfeeca36a3942fb54
Elyes <elyess.laiche@gmail.com>
2022-01-14 10:14:05 +0100

    Quest1 & 2

---
+++ TD1.js
@@ -0,0 +1,16 @@
+"use strict"
+// function Print1To10() {   // Quest1
+//     for (let i = 1 ; i <= 10; i++) {
+//         console.log(i)
+//     }
+// };
+
+// Print1To10()
+
+function PrintOddTo100() { // Quest2
+    for (let i = 1 ; i <= 50; i++) {
+        console.log(2*i)
+    }
+};
+
+PrintOddTo100()
